9361d5c4ec55362d4d6eb73b58a62c2ec5b470af
JustMili <106467462+JustMili0@users.noreply.github.com>
2026-06-28 16:03:41 +0200

    Change var name of the toast texture

--- src/main/java/net/justmili/reminders/client/gui/ReminderToast.java
+++ src/main/java/net/justmili/reminders/client/gui/ReminderToast.java
@@ -11,7 +11,7 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.ItemStack;
 
 public class ReminderToast implements Toast {
-    private static final Identifier SYSTEM = ResourceUtil.asMinecraft("toast/advancement");
+    private static final Identifier TOAST = ResourceUtil.asMinecraft("toast/advancement");
 
     private final Component title;
     private final Component message;
@@ -26,7 +26,7 @@ public class ReminderToast implements Toast {
 
     @Override
     public void render(GuiGraphics graphics, Font font, long visibleTime) {
-        graphics.blitSprite(RenderPipelines.GUI_TEXTURED, SYSTEM, 0, 0, width(), height());
+        graphics.blitSprite(RenderPipelines.GUI_TEXTURED, TOAST, 0, 0, width(), height());
 
         graphics.renderItem(icon, 8, 8, 0);
 
